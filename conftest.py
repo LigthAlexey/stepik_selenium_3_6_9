08c845dcf429567ac6efca901759c5afeeeec4eb
LigthAlexey <karalkin@mail.ru>
2022-06-05 19:21:17 +0700

    final test run

--- conftest.py
+++ conftest.py
@@ -1,32 +1,20 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default=None,
                      help="Choose user language")
 
+
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        options = Options()
-        options.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(options=options)
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    print("\nstart browser for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,9 +1,15 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+import time
 
 
-link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-# fff
-def test_guest_should_see_login_link(browser):
+
+def test_we_should_see_on_button_languages(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    time.sleep(30)
+    assert WebDriverWait(browser, 5).until(
+        ec.visibility_of_element_located((By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket"))
+    )
