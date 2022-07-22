550a1801331ad2c23209aed04aefaf8381a35089
LigthAlexey <karalkin@mail.ru>
2022-07-22 20:52:34 +0700

    coms

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-import time
+# import time
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
